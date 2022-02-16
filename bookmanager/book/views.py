f2530e35099847ae569a4073e85b4b93f36c54aa
wx0020 <4152443@qq.com>
2022-02-16 23:58:17 +0800

    增加路由

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# 视图函数
+# 参数1 接受请求 HttpRequest对象
+# 必须返回 相应对象 数据
+
+
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+def index(request):
+
+    return HttpResponse('ok')
